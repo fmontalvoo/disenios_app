1841e7120674d5d96692ebd065530a2b922c6e6c
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2020-12-18 17:17:14 -0500

    Cabecera triangular, punta y curva.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -42,7 +42,7 @@ class _HeaderDiagonalPainter extends CustomPainter {
 
     paint.color = Color(0xff615aab);
     paint.style = PaintingStyle.fill;
-    paint.strokeWidth = 2.5;
+    paint.strokeWidth = 1;
 
     final path = Path();
 
@@ -50,6 +50,121 @@ class _HeaderDiagonalPainter extends CustomPainter {
     path.lineTo(size.width, size.height * .25);
     path.lineTo(size.width, 0);
     path.lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class HeaderTriangular extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderTriangularPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderTriangularPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    paint.color = Color(0xff615aab);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 1;
+
+    final path = Path();
+
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+
+    // path.lineTo(size.width, size.height);
+    // path.lineTo(0, size.height);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class HeaderPunta extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderPuntaPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderPuntaPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    paint.color = Color(0xff615aab);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 1;
+
+    final path = Path();
+
+    path.lineTo(0, size.height * .25);
+    path.lineTo(size.width * .5, size.height * .3);
+    path.lineTo(size.width, size.height * .25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class HeaderCurvo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderCurvoPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderCurvoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    paint.color = Color(0xff615aab);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 1;
+
+    final path = Path();
+
+    path.lineTo(0, size.height * .25);
+    path.quadraticBezierTo(
+        size.width * .5, size.height * .5, size.width, size.height * .25);
+    path.lineTo(size.width, 0);
 
     canvas.drawPath(path, paint);
   }
